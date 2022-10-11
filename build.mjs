90ea4c7556f1b6e62dd858ec8b78baae74f0e1ab
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-10-11 16:01:24 -0600

    finalize fixing build issues

--- build.mjs
+++ build.mjs
@@ -15,7 +15,7 @@ build({
   target: "rhino1.7.14",
   external: ["kolmafia"],
   plugins: [babel()],
-  outdir: "KoLmafia",
+  outdir: "KoLmafia/scripts/",
   watch,
   loader: { ".json": "text" },
   inject: ["./kolmafia-polyfill.js"],
